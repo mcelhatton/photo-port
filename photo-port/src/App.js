1262d9d9f3e6710b650b01d733d889dea4fb03ed
mcelhatton <colinm@mobiledealerdata.com>
2022-07-06 16:50:26 -0500

    saving project files

---
+++ photo-port/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import Pet from './components/Pet';
+
+function App() {
+  return (
+    <Pet />
+  );
+}
+
+export default App;
---
+++ photo-port/src/components/Pet.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Pet = () => {
+  return (
+    <div>
+      <h1>Ginger</h1>
+      <p>Breed: Brittany Spaniel</p>
+    </div>
+  )
+}
+
+export default Pet;
