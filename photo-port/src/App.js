0e160d711da13bfae974f085d90195b29f7df7be
mcelhatton <colinm@mobiledealerdata.com>
2022-07-07 08:38:10 -0500

    saving working portfolio

--- photo-port/src/App.js
+++ photo-port/src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import './App.css';
-import Pet from './components/Pet';
+import Nav from './components/Nav';
+import About from './components/About';
 
 function App() {
+
   return (
-    <Pet />
+    <div>
+      <Nav />
+      <main>
+          <About />        
+      </main>
+    </div>
   );
 }
 
